cd73f55a24350a0b9ba2ed424938ed6979708282
Han <han@jvdiamondtech.com>
2021-10-20 20:39:02 +0800

    feat: posts crud without mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 
 /**
@@ -12,9 +13,21 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // 靜態頁面
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  // ejs
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Blog API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
