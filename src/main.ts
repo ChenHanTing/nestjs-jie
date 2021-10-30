13c11bdfd600e2250bb098d770b6ee2ca88d0d65
Han <han@jvdiamondtech.com>
2021-10-30 13:19:49 +0800

    feat: 跨網域請求

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ import { join } from 'path';
  *   https://newideas.coderbridge.io/2020/11/22/%E7%94%A8-nestjs-%E9%96%8B%E7%99%BC-api-%E5%90%A7-(%E4%BA%8C)-%E5%B0%88%E6%A1%88%E6%9E%B6%E6%A7%8B/
  */
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  /* 跨網域資源共享: https://morioh.com/p/bad87f42e5dd */
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
 
   // 靜態頁面
   app.useStaticAssets(join(__dirname, '..', 'public'));
